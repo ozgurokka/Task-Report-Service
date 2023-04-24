7c708a7f66f9b11dd034303050e5ed45f9bc011f
ozgur <ozgurokka2003@gmail.com>
2023-04-24 08:23:26 +0300

    initial commit

---
+++ src/main/java/com/getclayton/report/Task/Report/Service/TaskReportServiceApplication.java
@@ -0,0 +1,13 @@
+package com.getclayton.report.Task.Report.Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskReportServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskReportServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/getclayton/report/Task/Report/Service/TaskReportServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.getclayton.report.Task.Report.Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskReportServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
